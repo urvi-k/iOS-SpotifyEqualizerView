0285687687bec8b2560686698bac5a15c8c4c848
urvi koladiya <50900652+cp-urvi-k@users.noreply.github.com>
2020-12-17 18:43:14 +0530

    init project

---
+++ iOS-SpotifyEqualizerView/iOS_SpotifyEqualizerView.h
@@ -0,0 +1,18 @@
+//
+//  iOS_SpotifyEqualizerView.h
+//  iOS-SpotifyEqualizerView
+//
+//  Created by Urvi Koladiya on 26/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOS_SpotifyEqualizerView.
+FOUNDATION_EXPORT double iOS_SpotifyEqualizerViewVersionNumber;
+
+//! Project version string for iOS_SpotifyEqualizerView.
+FOUNDATION_EXPORT const unsigned char iOS_SpotifyEqualizerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_SpotifyEqualizerView/PublicHeader.h>
+
+
